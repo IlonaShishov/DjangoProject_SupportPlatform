8cb55f96fc6608fe3eafb763f8ce4d39a858b309
IlonaShishov <ilona.shishov@gmail.com>
2021-03-09 14:31:48 +0200

    insert data into case view and connect equipment list

--- ASISupport_app/views.py
+++ ASISupport_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from ASISupport_app.models import Case, Visit
+from ASISupport_app.models import Case, Visit, Employee, Customer, CaseEquipment, Equipment
 # from django.views.generic import (TemplateView)
 # from ASISupport_app.models import Case, Visit
 
@@ -36,17 +36,29 @@ def dashboard_view(request):
 
 def new_case_view(request):
 	state = 'new'
+	types = Case.TYPES
+	statuses = Case.STATUSES
+	employees = Employee.objects.all()
+	customers = Customer.objects.all()
 	return render(request, 'ASISupport_app/case.html', locals())
 
 def case_view(request, id):
 	state = 'view'
 	case = Case.objects.get(case_num=id)
 	visits = Visit.objects.filter(case_num=id)
+
+	case_equipment_lst = CaseEquipment.objects.filter(case_num=id)
+	equip_lst = [ce.equip_sn for ce in case_equipment_lst]
+	equipment = Equipment.objects.filter(equip_sn__in=equip_lst)
+	
+	statuses = Case.STATUSES
 	return render(request, 'ASISupport_app/case.html', locals())
 
 def new_visit_view(request):
+	state = 'new'
 	return render(request, 'ASISupport_app/visit.html', locals())
 
 def visit_view(request, id):
+	state = 'view'
 	visit = Visit.objects.get(visit_num=id)
 	return render(request, 'ASISupport_app/visit.html', locals())
